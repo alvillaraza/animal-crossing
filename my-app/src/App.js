897fd8c37dee97f8a7afe0ac8de672eec1ce710c
alexis villaraza <alexis.borja@gmail.com>
2020-10-12 17:16:24 -0700

    using forms to edit header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import Form from './components/Form';
-import './App.css';
+import React, { useState } from "react";
+// import Form from './components/Form';
+import "./App.css";
 
 function App() {
-  // const [newHeader, setNewHeader] = useState()
+  const [newHeader, setNewHeader] = useState("Some Header");
+
+  const handleChanges = (e) => {
+    setNewHeader(e.target.value);
+    console.log(newHeader);
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   console.log(newHeader);
+  //   //TO DO: make new header be the submitted item in form
+  //   setNewHeader(newHeader)
+  // };
 
   return (
     <div>
-     <Form />
+      <div>
+        <h1>{newHeader}</h1>
+        {/* <form> */}
+        New Header: <input type="text" name="header" value={newHeader} onChange={handleChanges} />
+      
+        <p><select onChange={handleChanges}>
+          <option>Some Stuff</option>
+          <option>Other Things</option>
+          <option>Another option</option>
+        </select></p>
+
+        <input type='checkbox' value='some other value' onChange={handleChanges} />{newHeader}
+
+
+       <p> <button
+          onClick={() => {
+            setNewHeader("blah");
+          }}
+        >
+          Change to blah
+        </button></p>
+      </div>
     </div>
   );
 }
--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,20 +1,13 @@
 import React from "react";
 
-function Form() {
-  const handleChanges = (e) => {
-    console.log(e.target.value);
-  };
+function Form({handleSubmit, handleChanges}) {
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("submitted");
-  };
 
   return (
     <div>
       <h1>Header here</h1>
       <form onSubmit={handleSubmit}>
-        New Input: <input type="text" name="header" onChange={handleChanges} />
+        New Header: <input type="text" name="header" onChange={handleChanges} />
         <p>
           <button>Submit</button>
         </p>
