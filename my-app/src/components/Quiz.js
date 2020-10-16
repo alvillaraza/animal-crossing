07a1f34f072e0bd4d2b9b39b390d17d9c960873a
alexis villaraza <alexis.borja@gmail.com>
2020-10-16 10:59:32 -0700

    styled quiz

--- my-app/src/components/Quiz.js
+++ my-app/src/components/Quiz.js
@@ -1,26 +1,49 @@
 import React, { useState } from "react";
 import QuizData from "../data";
+import "../App.css";
 
 function App() {
-  const handleChanges = (e) => {
-    // setAnswer(e.target.value);
-    // console.log("this answer =", answer);
-    // console.log("e.target.value =", e.target.value);
-  };
+  const [answered, setAnswered] = useState(false);
+  const [response, setResponse] = useState(null);
+
+  console.log(response);
 
   return (
     <div>
       {QuizData.map((question) => {
+        // console.log("question", question);
         return (
-          <div>
-            <h3> {question.Question}</h3>
-            <h2>
-              <input type="radio" name="choice" onChange={handleChanges} />
-              {question.Answer1}
+          <div className="question-container">
+            <p className="question-image-container">
+              <img src={question.image} />
+            </p>
+            <h3 className="question"> {question.question}</h3>
+            <h2 className="answers-container">
+              {question.answers.map((answer) => {
+                return (
+                  <div>
+                    <button
+                      className="answer-button-container"
+                      onClick={() => {
+                        answer.isCorrect
+                          ? setResponse(true)
+                          : setResponse(false);
+                        setAnswered(true);
+                      }}
+                    >
+                      {answer.answer}
+                    </button>
+                  </div>
+                );
+              })}
             </h2>
-            <h2><input type="radio" name="choice" onChange={handleChanges} />{question.Answer2}</h2>
-            <h2><input type="radio" name="choice" onChange={handleChanges} />{question.Answer3}</h2>
-            <h2><input type="radio" name="choice" onChange={handleChanges} />{question.Answer4}</h2>
+            <div
+              className={`validation-container
+            ${answered ? "show" : "hide"}
+            ${response ? "result-correct" : "result-incorrect"}`}
+            >
+              {response === true ? "Answer Correct" : "Wrong"}
+            </div>
           </div>
         );
       })}
--- my-app/src/data.js
+++ my-app/src/data.js
@@ -1,19 +1,78 @@
+import q1 from "./images/q1.jpg";
+import q2 from "./images/q2.jpg";
+import q3 from "./images/q3.jpg";
+import q4 from "./images/q4.png";
+import q5 from "./images/q5.jpg";
+import q6 from "./images/q6.jpg";
+import q7 from "./images/q7.jpg";
+
 const QuizData = [
   {
-    Question: "Which of the following names is NOT one of the Nook clan?",
-    Answer1: "Timmy",
-    Answer2: "Tommy",
-    Answer3: "Tammy",
-    Answer4: "Tom",
+    image: q1,
+    question: "Which of the following names is NOT one of the Nook clan?",
+    answers: [
+      { answer: "Timmy", isCorrect: false },
+      { answer: "Tommy", isCorrect: false },
+      { answer: "Tammy", isCorrect: true },
+      { answer: "Tom", isCorrect: false },
+    ],
   },
-  {
-    Question:
+  {image: q2,
+    question:
       "Not only are some of Crazy Redd's paintings fake, but they're haunted too! What does the girl in the haunted Wistful Painting do?",
-    Answer1: "She winks at you",
-    Answer2: "She picks her nose",
-    Answer3: "Her earring falls off",
-    Answer4: "She closes her eyes",
+      answers: [
+        { answer: "She winks at you", isCorrect: false },
+        { answer: "She picks her nose", isCorrect: false },
+        { answer: "Her earring falls off", isCorrect: false },
+        { answer: "She closes her eyes", isCorrect: true },
+      ],
   },
+  {image: q3,
+    question: "Which of these bugs is NOT available to catch in Animal Crossing: New Horizons?",
+    answers: [
+      { answer: "Titan Beetle", isCorrect: true },
+      { answer: "Rice Grasshopper", isCorrect: false },
+      { answer: "Wasp", isCorrect: false },
+      { answer: "Giant Waterbug", isCorrect: false },
+    ],
+  },
+  {image: q4,
+    question: "Bunny Day has been and gone this year, but what’s the name of the rabbit who runs it?",
+    answers: [
+      { answer: "Bumper", isCorrect: false },
+      { answer: "Thumper", isCorrect: false },
+      { answer: "Tipper", isCorrect: false },
+      { answer: "Zipper", isCorrect: true },
+    ],
+    
+  },
+  {image: q5,
+    question: "Which of the following is NOT a fruit you can grow in New Horizons?",
+    answers: [
+      { answer: "Coconuts", isCorrect: false },
+      { answer: "Cherries", isCorrect: false },
+      { answer: "Kiwis", isCorrect: true },
+      { answer: "Pears", isCorrect: false },
+    ],
+  },
+  {image: q6,
+    question: "What's the name of the turnip seller who visits your island on Sunday mornings?",
+    answers: [
+      { answer: "Daisy Mae", isCorrect: true },
+      { answer: "Mary Sue", isCorrect: false },
+      { answer: "Sonny Jim", isCorrect: false },
+      { answer: "Tina Turnip", isCorrect: false },
+    ],
+  },
+  {image: q7,
+    question: "In total, how many fish are there to catch in Animal Crossing: New Horizons?",
+    answers: [
+      { answer: "75", isCorrect: true },
+      { answer: "80", isCorrect: true },
+      { answer: "90", isCorrect: false },
+      { answer: "100", isCorrect: false },
+    ],
+  }
 ];
 
 export default QuizData;
